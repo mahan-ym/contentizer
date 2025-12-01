607591fb14950baa2e5b8b4c93f31a24dbd6a2b1
mahan_ym <mahan.yarmohammadtajari@studenti.unipd.it>
2025-12-01 11:35:04 +0100

    added freepik image generation function using flux

--- back/src/tools/freepik.py
+++ back/src/tools/freepik.py
@@ -106,19 +106,100 @@ def gen_vid(
                 )
     else:
         print(
+            f"Error while generating the video. Status code: {response.status_code}, message: {response.json()['message']}"
+        )
+
+
+def gen_image(
+    prompt: str,
+    aspect_ratio: str = "square_1_1",
+):
+    """
+    
+    args:
+        prompt (str): The prompt to guide the image generation.
+        aspect_ratio (str): The aspect ratio of the generated image. 
+            Available options: square_1_1, classic_4_3, traditional_3_4, widescreen_16_9, social_story_9_16, standard_3_2, portrait_2_3, horizontal_2_1, vertical_1_2, social_post_4_5 
+            Example: "square_1_1"
+    """
+    endpoint = f"{BASE_URL}/ai/text-to-image/flux-pro-v1-1"
+    headers = {
+        "x-freepik-api-key": f"{FREEPIK_API_KEY}",
+        "Content-Type": "application/json",
+    }
+
+    payload = {
+        "prompt": prompt,
+        "prompt_upsampling": True,
+        "aspect_ratio": aspect_ratio,
+        "safety_tolerance": 2,
+        "output_format": "jpeg",
+    }
+
+    response = requests.post(endpoint, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        task_id = response.json()["data"]["task_id"]
+        status = response.json()["data"]["status"]
+
+        start_time = time.time()
+        generation_ok = True
+        # Polling until the status is "COMPLETED"
+        while status != "COMPLETED":
+            print(f"Waiting for the task to complete... (current status: {status})")
+            time.sleep(2)  # Wait 2 seconds before checking again
+            status_url = (
+                f"{BASE_URL}/ai/text-to-image/flux-pro-v1-1/{task_id}"
+            )
+            response = requests.get(
+                status_url,
+                headers={"x-freepik-api-key": f"{FREEPIK_API_KEY}"},
+            )
+            if response.status_code == 200:
+                status = response.json()["data"]["status"]
+            else:
+                print(f"Error while checking the task status: {response.status_code}")
+                time.sleep(2)
+            if time.time() - start_time > timeout:
+                print("Timeout reached")
+                generation_ok = False
+                break
+
+        if generation_ok:
+            print("COMPLETED")
+            # Download the image ---------------------------------------------------------------
+            IMAGE_URL = response.json()["data"]["generated"][0]
+            current_dir = os.path.dirname(os.path.abspath(__file__))
+            file_name = os.path.join(current_dir, "generated_image.jpeg")
+            image_response = requests.get(IMAGE_URL)
+            if image_response.status_code == 200:
+                with open(file_name, "wb") as f:
+                    f.write(image_response.content)
+                print(f"Image successfully downloaded as {file_name}")
+            else:
+                print(
+                    f"Could not download the image. Status code: {image_response.status_code}"
+                )
+    else:
+        print(
             f"Error while generating the image. Status code: {response.status_code}, message: {response.json()['message']}"
         )
 
 
-# if __name__ == "__main__":
-#     # image_URL = "https://img.b2bpic.net/premium-photo/portrait-smiling-senior-woman-blue-vintage-convertible_220770-28364.jpg"
-#     current_dir = os.path.dirname(os.path.abspath(__file__))
-#     with open(os.path.join(current_dir, "s-bg3.jpg"), "rb") as image_file:
-#         image_base64 = base64.b64encode(image_file.read()).decode("utf-8")
 
-#         gen_vid(
-#             image=f"data:image/jpeg;base64,{image_base64}",
-#             prompt="create a shock and make the stones float into the sky. create a blue light around the stones when they are floated.",
-#             negative_prompt="blurry, low resolution, dark, gloomy, cartoon",
-#             duration=5,
-#         )
+
+if __name__ == "__main__":
+    # image_URL = "https://img.b2bpic.net/premium-photo/portrait-smiling-senior-woman-blue-vintage-convertible_220770-28364.jpg"
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # gen_vid(
+    #     image= os.path.join(current_dir, "s-bg3.jpg"),
+    #     prompt="create a shock and make the stones float into the sky. create a blue light around the stones when they are floated.",
+    #     negative_prompt="blurry, low resolution, dark, gloomy, cartoon",
+    #     duration=5,
+    # )
+
+    gen_image(
+        prompt="A Beautiful animated wrench flying toward the a bolt to fasten it, high detail, vibrant colors, dynamic lighting, dramatic angle, digital art",
+        aspect_ratio="widescreen_16_9",
+    )
